4bd29668d08098c5b7dc569740df34107975d5f8
Massimo Rondelli <m.rondelli@cineca.it>
2023-06-13 12:41:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-document.getElementById("button-click-me").addEventListener("click", () => {
-    alert("You clicked me")
-});
+// document.getElementById("button-click-me").addEventListener("click", () => {
+//     alert("You clicked me")
+// });
+
+const button = document.getElementById("button-click-me");
+if(button){
+    button.addEventListener("click", () => {
+        alert("You clicked me");
+    })
+}
